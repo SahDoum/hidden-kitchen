6287b70bde2f020eca6507b0467563edef7c8c74
sahdoum <sahdoum@proton.me>
2023-06-11 23:21:01 +0400

    Heroku test

--- HiddenKitchen/urls.py
+++ HiddenKitchen/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+def test(request):
+    return "Hello world!"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test),
 ]
