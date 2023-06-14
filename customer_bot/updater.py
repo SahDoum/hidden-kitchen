6c8406f8e9622cf8fcb8b2b5e9712ab32908256a
sahdoum <sahdoum@proton.me>
2023-06-14 19:59:17 +0400

    fix

--- customer_bot/updater.py
+++ customer_bot/updater.py
@@ -3,11 +3,8 @@ from telegram.ext import Updater
 from HiddenKitchen.settings import CUSTOMER_BOT_TOKEN
 
 
-print("STARTING POLLING!!!!")
-print(CUSTOMER_BOT_TOKEN)
+# updater = Updater(token=CUSTOMER_BOT_TOKEN, use_context=True)
+# dispatcher = updater.dispatcher
 
-updater = Updater(token=CUSTOMER_BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-dispatcher.add_handler(CommandHandler("start", start_command))
-updater.start_polling()
+# dispatcher.add_handler(CommandHandler("start", start_command))
+# updater.start_polling()
