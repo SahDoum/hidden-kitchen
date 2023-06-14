3bcff235087f24b5e655f3ea3bdd724a8813e652
sahdoum <sahdoum@proton.me>
2023-06-14 20:05:21 +0400

    check

--- notifications/bot.py
+++ notifications/bot.py
@@ -1,6 +1,6 @@
 import telegram
 from django.conf import settings
 
-customer_bot = telegram.Bot(token=settings.CUSTOMER_BOT_TOKEN) if settings.CUSTOMER_BOT_TOKEN else None
+customer_bot = None # telegram.Bot(token=settings.CUSTOMER_BOT_TOKEN) if settings.CUSTOMER_BOT_TOKEN else None
 kitchen_bot = telegram.Bot(token=settings.KITCHEN_BOT_TOKEN) if settings.KITCHEN_BOT_TOKEN else None
 courier_bot = telegram.Bot(token=settings.COURIER_BOT_TOKEN) if settings.COURIER_BOT_TOKEN else None
